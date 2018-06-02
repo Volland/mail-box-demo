cd30af19427fa3eead80265d8b6571f165f6f496
Volodymyr Pavlyshyn <pavlyshyn@gmail.com>
2018-06-02 20:25:56 +0200

    GH-9 Add a basic security and routes setup

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const pinoLoggerMiddleware = require('express-pino-logger');
+const pino = require('pino')();
+const yargs = require('yargs');
+const apiRoutes = require('./routes/api-routes');
+
+const defaultOptions = {
+    port: 8080,
+    host: 'mailbox.com',
+};
+
+const commandLineOptions = yargs.default(defaultOptions).argv;
+
+const server = express();
+// add logging middleware
+server.use(pinoLoggerMiddleware());
+
+server.get('/health', (req, res ) => res.json({ok: true}));
+
+apiRoutes(server);
+
+server.listen(commandLineOptions.port, () => {
+    pino.info(` Server started on port ${commandLineOptions.port}`);
+});
+
+
+
