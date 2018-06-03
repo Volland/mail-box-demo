7572e1fa9df083333a05e30af18e63ae1347d88e
Volodymyr Pavlyshyn <pavlyshyn@gmail.com>
2018-06-03 21:08:18 +0200

    GH-2 Implement export task . fill a db with a init data

---
+++ db/scripts/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS public.messages
+(
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    subject varchar(200) NOT NULL,
+    send_at INTEGER NOT NULL,
+    sender varchar(200) NOT NULL,
+    message text NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS index_messages_on_id ON public.messages USING btree (id);
+CREATE INDEX IF NOT EXISTS index_messages_on_send_at ON public.messages USING btree (send_at);
