039033bdfdabcf0dbb72a167055c1e90609858ff
Volodymyr Pavlyshyn <pavlyshyn@gmail.com>
2018-06-10 20:34:14 +0200

    GH-0 add missed swagger endpoints

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const pino = require('pino')();
 const pinoLoggerMiddleware = require('express-pino-logger');
 const yargs = require('yargs');
 const apiRoutes = require('./routes/api-routes');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 const defaultOptions = {
     port: 8080,
@@ -17,7 +19,11 @@ server.use(pinoLoggerMiddleware());
 
 server.get('/health', (req, res ) => res.json({ok: true}));
 
+
 apiRoutes(server);
+
+const swaggerDocument = YAML.load('./api/mail-box-api.yaml');
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 server.listen(commandLineOptions.port, () => {
     pino.info(` Server started on port ${commandLineOptions.port}`);
